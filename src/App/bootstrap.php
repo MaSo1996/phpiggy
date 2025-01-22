9ebed6caf41746d1abcf5fd4d7358cce64de692c
MaSo1996 <marcin.sochacki.programista@gmail.com>
2025-01-22 16:01:47 +0100

    Next step in course

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
+
+echo HomeController::class;
 
 $app = new App();
 
-$app->get('/');
-
-dd($app);
+$app->get('/', [HomeController::class, 'home']);
 
 return $app;
