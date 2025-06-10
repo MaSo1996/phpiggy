696212c1b0c12ea13b211b62dcad478708390138
MaSo1996 <marcin.sochacki.programista@gmail.com>
2025-06-10 15:56:45 +0200

    New section

--- src/Framework/Container.php
+++ src/Framework/Container.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Framework;
 
+use ReflectionClass;
+use Framework\Exceptions\ContainerException;
+
 class Container
 {
   private array $definitions = [];
@@ -11,7 +14,16 @@ class Container
   public function addDefinitions(array $newDefinitions)
   {
     $this->definitions = [...$this->definitions, ...$newDefinitions];
+  }
 
-    dd($this->definitions);
+  public function resolve(string $className)
+  {
+    $reflectionClass = new ReflectionClass($className);
+
+    if (!$reflectionClass->isInstantiable()) {
+      throw new ContainerException("Classe {$className} is not instantiable");
+    }
+
+    dd($reflectionClass);
   }
 }
